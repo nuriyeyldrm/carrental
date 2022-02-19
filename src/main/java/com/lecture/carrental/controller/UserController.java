642339e4b44500e7b03216f511c1475f6a6b7696
Nuriye <nry95yldrm@gmail.com>
2022-02-20 00:34:51 +0300

    initial commit

--- src/main/java/com/lecture/carrental/controller/UserController.java
+++ src/main/java/com/lecture/carrental/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.lecture.carrental.controller;
 
 import com.lecture.carrental.domain.User;
+import com.lecture.carrental.security.jwt.JwtUtils;
 import com.lecture.carrental.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,8 @@ import java.util.Map;
 public class UserController {
 
     public UserService userService;
+    public AuthenticationManager authenticationManager;
+    public JwtUtils jwtUtils;
 
 
     @PostMapping("/register")
@@ -34,4 +41,23 @@ public class UserController {
 
         return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> authenticateUser(@RequestBody Map<String, Object> userMap){
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+
+        userService.login(email, password);
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", jwt);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/lecture/carrental/service/UserService.java
+++ src/main/java/com/lecture/carrental/service/UserService.java
@@ -3,16 +3,19 @@ package com.lecture.carrental.service;
 import com.lecture.carrental.domain.Role;
 import com.lecture.carrental.domain.User;
 import com.lecture.carrental.domain.enumeration.UserRole;
+import com.lecture.carrental.exception.AuthException;
 import com.lecture.carrental.exception.BadRequestException;
 import com.lecture.carrental.exception.ConflictException;
 import com.lecture.carrental.exception.ResourceNotFoundException;
 import com.lecture.carrental.repository.RoleRepository;
 import com.lecture.carrental.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -41,4 +44,25 @@ public class UserService {
         user.setRoles(roles);
         userRepository.save(user);
     }
+
+    public void login(String email, String password) throws AuthException {
+        try {
+            Optional<User> user = userRepository.findByEmail(email);
+
+            if (!BCrypt.checkpw(password, user.get().getPassword()))
+                throw new AuthException("invalid credentials");
+
+        }catch (Exception e){
+            throw new AuthException("invalid credentials");
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
